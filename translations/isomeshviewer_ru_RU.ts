b59fbaebe6322a0d36cc06d1420da07215422674
Nikita Sirgienko <warquark@gmail.com>
2019-04-21 23:25:26 +0300

    GIT_SILENT Update Russian translation

--- translations/isomeshviewer_ru_RU.ts
+++ translations/isomeshviewer_ru_RU.ts
@@ -5,13 +5,13 @@
     <name>MainWindow</name>
     <message>
         <location filename="../src/mainwindow.ui" line="17"/>
-        <location filename="../src/mainwindow.cpp" line="182"/>
-        <location filename="../src/mainwindow.cpp" line="186"/>
-        <location filename="../src/mainwindow.cpp" line="295"/>
-        <location filename="../src/mainwindow.cpp" line="373"/>
-        <location filename="../src/mainwindow.cpp" line="390"/>
-        <location filename="../src/mainwindow.cpp" line="403"/>
-        <location filename="../src/mainwindow.cpp" line="423"/>
+        <location filename="../src/mainwindow.cpp" line="185"/>
+        <location filename="../src/mainwindow.cpp" line="189"/>
+        <location filename="../src/mainwindow.cpp" line="298"/>
+        <location filename="../src/mainwindow.cpp" line="376"/>
+        <location filename="../src/mainwindow.cpp" line="393"/>
+        <location filename="../src/mainwindow.cpp" line="406"/>
+        <location filename="../src/mainwindow.cpp" line="427"/>
         <source>Isomesh Viewer</source>
         <translation></translation>
     </message>
@@ -173,14 +173,14 @@
         <translation>&amp;Минимум высоты</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1247"/>
+        <location filename="../src/mainwindow.ui" line="1254"/>
         <source>Export mesh to .ply</source>
         <translation>Экспортировать мэш в .ply</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="578"/>
         <location filename="../src/mainwindow.ui" line="684"/>
-        <location filename="../src/mainwindow.ui" line="1222"/>
+        <location filename="../src/mainwindow.ui" line="1229"/>
         <source>Choose file</source>
         <translation>Выбор файла</translation>
     </message>
@@ -200,112 +200,117 @@
         <translation>Показывать оригинал</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="718"/>
+        <location filename="../src/mainwindow.ui" line="701"/>
+        <source>Revert normals</source>
+        <translation>Вывернуть нормали</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="725"/>
         <source>&amp;Algorithm</source>
         <translation>&amp;Алгоритм</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="749"/>
+        <location filename="../src/mainwindow.ui" line="756"/>
         <source>Algorithm parameters</source>
         <translation>Параметры алгоритма</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="778"/>
+        <location filename="../src/mainwindow.ui" line="785"/>
         <source>Simplification tolerance</source>
         <translation>Чувствительность упрощения</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="785"/>
+        <location filename="../src/mainwindow.ui" line="792"/>
         <source>Use adaptive simplification</source>
         <translation>Использовать адаптивное упрощение</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="800"/>
+        <location filename="../src/mainwindow.ui" line="807"/>
         <source>A&amp;ccuracy</source>
         <translation>&amp;Точность</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="826"/>
+        <location filename="../src/mainwindow.ui" line="833"/>
         <source>Use simple split heuristic</source>
         <translation>Простая эвристика разделения</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="833"/>
+        <location filename="../src/mainwindow.ui" line="840"/>
         <source>Use early split stop heuristic</source>
         <translation>Эвристика раннего разделения</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="840"/>
+        <location filename="../src/mainwindow.ui" line="847"/>
         <source>Use random sampling</source>
         <translation>Использовать случайные точки</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="865"/>
+        <location filename="../src/mainwindow.ui" line="872"/>
         <source>Common parameters</source>
         <translation>Общие параметры</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="878"/>
+        <location filename="../src/mainwindow.ui" line="885"/>
         <source>Grid</source>
         <translation>Сетка</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="886"/>
+        <location filename="../src/mainwindow.ui" line="893"/>
         <source>Gr&amp;id size</source>
         <translation>Размер се&amp;тки</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="933"/>
+        <location filename="../src/mainwindow.ui" line="940"/>
         <source>256</source>
         <translation>256</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="938"/>
+        <location filename="../src/mainwindow.ui" line="945"/>
         <source>512</source>
         <translation>512</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="946"/>
+        <location filename="../src/mainwindow.ui" line="953"/>
         <source>G&amp;rid step</source>
         <translation>&amp;Шаг сетки</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="978"/>
+        <location filename="../src/mainwindow.ui" line="985"/>
         <source>X &amp;offset</source>
         <translation>Смещение по &amp;X</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1030"/>
+        <location filename="../src/mainwindow.ui" line="1037"/>
         <source>Z offs&amp;et</source>
         <translation>Смещение по &amp;Z</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1059"/>
+        <location filename="../src/mainwindow.ui" line="1066"/>
         <source>Lighting</source>
         <translation>Освещение</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1067"/>
+        <location filename="../src/mainwindow.ui" line="1074"/>
         <source>Lighting &amp;direction X</source>
         <translation>Направление освещения &amp;X</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1093"/>
+        <location filename="../src/mainwindow.ui" line="1100"/>
         <source>Lighting direction &amp;Y</source>
         <translation>Направление освещения &amp;Y</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1119"/>
+        <location filename="../src/mainwindow.ui" line="1126"/>
         <source>Lighting direction &amp;Z</source>
         <translation>Направление освещения &amp;Z</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1145"/>
+        <location filename="../src/mainwindow.ui" line="1152"/>
         <source>Use Lighting</source>
         <translation>Использовать освещение</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1155"/>
+        <location filename="../src/mainwindow.ui" line="1162"/>
         <source>Use color from normal</source>
         <translation>Использовать цвета по нормалям</translation>
     </message>
@@ -313,7 +318,7 @@
         <location filename="../src/mainwindow.ui" line="133"/>
         <location filename="../src/mainwindow.ui" line="156"/>
         <location filename="../src/mainwindow.ui" line="179"/>
-        <location filename="../src/mainwindow.ui" line="968"/>
+        <location filename="../src/mainwindow.ui" line="975"/>
         <source>1</source>
         <translation></translation>
     </message>
@@ -335,18 +340,18 @@
         <location filename="../src/mainwindow.ui" line="533"/>
         <location filename="../src/mainwindow.ui" line="550"/>
         <location filename="../src/mainwindow.ui" line="604"/>
-        <location filename="../src/mainwindow.ui" line="819"/>
-        <location filename="../src/mainwindow.ui" line="971"/>
-        <location filename="../src/mainwindow.ui" line="1202"/>
+        <location filename="../src/mainwindow.ui" line="826"/>
+        <location filename="../src/mainwindow.ui" line="978"/>
+        <location filename="../src/mainwindow.ui" line="1209"/>
         <source>positive real number</source>
         <translation>положительное вещественное число</translation>
     </message>
     <message>
         <location filename="../src/mainwindow.ui" line="505"/>
         <location filename="../src/mainwindow.ui" line="653"/>
-        <location filename="../src/mainwindow.ui" line="994"/>
-        <location filename="../src/mainwindow.ui" line="1020"/>
-        <location filename="../src/mainwindow.ui" line="1046"/>
+        <location filename="../src/mainwindow.ui" line="1001"/>
+        <location filename="../src/mainwindow.ui" line="1027"/>
+        <location filename="../src/mainwindow.ui" line="1053"/>
         <source>0</source>
         <translation></translation>
     </message>
@@ -357,161 +362,161 @@
         <location filename="../src/mainwindow.ui" line="508"/>
         <location filename="../src/mainwindow.ui" line="630"/>
         <location filename="../src/mainwindow.ui" line="656"/>
-        <location filename="../src/mainwindow.ui" line="997"/>
-        <location filename="../src/mainwindow.ui" line="1023"/>
-        <location filename="../src/mainwindow.ui" line="1049"/>
-        <location filename="../src/mainwindow.ui" line="1086"/>
-        <location filename="../src/mainwindow.ui" line="1112"/>
-        <location filename="../src/mainwindow.ui" line="1138"/>
+        <location filename="../src/mainwindow.ui" line="1004"/>
+        <location filename="../src/mainwindow.ui" line="1030"/>
+        <location filename="../src/mainwindow.ui" line="1056"/>
+        <location filename="../src/mainwindow.ui" line="1093"/>
+        <location filename="../src/mainwindow.ui" line="1119"/>
+        <location filename="../src/mainwindow.ui" line="1145"/>
         <source>real number</source>
         <translation>вещественное число</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1004"/>
+        <location filename="../src/mainwindow.ui" line="1011"/>
         <source>&amp;Y offset</source>
         <translation>Смещение по &amp;Y</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1165"/>
+        <location filename="../src/mainwindow.ui" line="1172"/>
         <source>Texture</source>
         <translation>Текстура</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1173"/>
+        <location filename="../src/mainwindow.ui" line="1180"/>
         <source>Use texture</source>
         <translation>Использовать текстуру</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1183"/>
+        <location filename="../src/mainwindow.ui" line="1190"/>
         <source>T&amp;exture scale</source>
         <translation>Маштаб &amp;текстуры</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1209"/>
+        <location filename="../src/mainwindow.ui" line="1216"/>
         <source>Te&amp;xture file</source>
         <translation>Файл те&amp;кстуры</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.ui" line="1240"/>
+        <location filename="../src/mainwindow.ui" line="1247"/>
         <source>Generate mesh</source>
         <translation>Сгенерировать меш</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="133"/>
+        <location filename="../src/mainwindow.cpp" line="136"/>
         <source>Plane</source>
         <translation>Плоскость</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="134"/>
+        <location filename="../src/mainwindow.cpp" line="137"/>
         <source>Ellipsoid</source>
         <translation>Эллипсоид</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="135"/>
+        <location filename="../src/mainwindow.cpp" line="138"/>
         <source>Box</source>
         <translation>Параллелепипед</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="136"/>
+        <location filename="../src/mainwindow.cpp" line="139"/>
         <source>Waves</source>
         <translation>Волны</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="137"/>
+        <location filename="../src/mainwindow.cpp" line="140"/>
         <source>Perlin noise</source>
         <translation>Шум Перлина</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="138"/>
+        <location filename="../src/mainwindow.cpp" line="141"/>
         <source>Multifractal</source>
         <translation>Мультифрактал</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="139"/>
+        <location filename="../src/mainwindow.cpp" line="142"/>
         <source>Two spheres</source>
         <translation>Две сферы</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="140"/>
+        <location filename="../src/mainwindow.cpp" line="143"/>
         <source>Heightmap</source>
         <translation>Карта высот</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="141"/>
+        <location filename="../src/mainwindow.cpp" line="144"/>
         <source>Model</source>
         <translation>Трёхмерная Модель</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="147"/>
+        <location filename="../src/mainwindow.cpp" line="150"/>
         <source>Marching Cubes</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="148"/>
+        <location filename="../src/mainwindow.cpp" line="151"/>
         <source>Dual Contouring</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="149"/>
+        <location filename="../src/mainwindow.cpp" line="152"/>
         <source>Dual Marching Cubes</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="182"/>
+        <location filename="../src/mainwindow.cpp" line="185"/>
         <source>Heigthmap file not loaded</source>
         <translation>Файл карты высот не загружен</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="186"/>
+        <location filename="../src/mainwindow.cpp" line="189"/>
         <source>Model file not loaded</source>
         <translation>Файл модели не загружен</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="296"/>
-        <location filename="../src/mainwindow.cpp" line="374"/>
+        <location filename="../src/mainwindow.cpp" line="299"/>
+        <location filename="../src/mainwindow.cpp" line="377"/>
         <source>Some inputs fields contains invalid information or empty (the fields with invalid input marks by red color)</source>
         <translation>Некоторые поля ввода содержат неправильную информацию или пусты (поля с неправильньным вводом отмечены красным цветом)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="384"/>
+        <location filename="../src/mainwindow.cpp" line="387"/>
         <source>Load heightmap</source>
         <translation>Загрузка карты высот</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="384"/>
+        <location filename="../src/mainwindow.cpp" line="387"/>
         <source>Heightmap Files (*.png *.jpg *.bmp)</source>
         <translation>Карты высот (*.png *.jpg *.bmp)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="390"/>
-        <location filename="../src/mainwindow.cpp" line="403"/>
-        <location filename="../src/mainwindow.cpp" line="423"/>
+        <location filename="../src/mainwindow.cpp" line="393"/>
+        <location filename="../src/mainwindow.cpp" line="406"/>
+        <location filename="../src/mainwindow.cpp" line="427"/>
         <source>Loading file ends with error: %1</source>
         <translation>Загрузка файла завершилось ошибкой: %1</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="396"/>
+        <location filename="../src/mainwindow.cpp" line="399"/>
         <source>Load Texture</source>
         <translation>Загрузка текстуры</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="396"/>
+        <location filename="../src/mainwindow.cpp" line="399"/>
         <source>Texture Files (*.png *.jpg *.bmp)</source>
         <translation>Файл текстуры (*png *.jpg *.bmp)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="409"/>
+        <location filename="../src/mainwindow.cpp" line="412"/>
         <source>Load Model</source>
         <translation>Загрузка модели</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="409"/>
-        <location filename="../src/mainwindow.cpp" line="429"/>
+        <location filename="../src/mainwindow.cpp" line="412"/>
+        <location filename="../src/mainwindow.cpp" line="445"/>
         <source>Models Files (*.ply)</source>
         <translation>Файл модели (*.ply)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="429"/>
+        <location filename="../src/mainwindow.cpp" line="445"/>
         <source>Save Mesh</source>
         <translation>Сохранить мэш</translation>
     </message>
